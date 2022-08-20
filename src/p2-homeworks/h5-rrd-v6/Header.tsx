cbe294003af9d9a2e1e2910670525c21973eed4c
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-08-20 22:51:53 +0300

    complete hw5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import './HW5.css'
 
 function Header() {
+    const navLinkStyleHandler = (isActive: boolean) => isActive ? "activeLink" : "defaultLink"
     return (
-        <div>
-            // add NavLinks
-
+        <div className={"headerContainer"}>
+            <NavLink className={({isActive}) => navLinkStyleHandler(isActive)}
+                     to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+            <NavLink className={({isActive}) => navLinkStyleHandler(isActive)}
+                     to={PATH.JUNIOR}>junior</NavLink>
+            <NavLink className={({isActive}) => navLinkStyleHandler(isActive)}
+                     to={PATH.JUNIOR_PLUS}>junior_plus</NavLink>
+            <div className={"menuStyle"}>Menu</div>
         </div>
     )
 }
