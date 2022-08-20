c32c6a836b9a021df3b4f42215b96eb3bdd24f24
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-08-20 23:45:35 +0300

    complete hw5

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../HW5.css'
 
 function Error404() {
+
     return (
-        <div>
+        <div className={"errorContainer"}>
             <div>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
